81ba0f93fa0f0e42fbc45eea68370d759f82eb73
hahas507 <aq12361@gmail.com>
2021-04-05 20:50:27 +0900

    03차시

---
+++ UnityCS/02NameSpace/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//프로그래머가 2명이다.
+// A프로그래머
+// HP포션
+// B프로그래머
+// MP포션
+
+// 클래스의 이름들은 식별자라고 한다.
+// 어떤 코드적 내용을 묶는 하나의 명칭이다.
+
+namespace APRO
+{
+    internal class /*APRO*/ Potion
+    {
+        // A프로그래머
+    }
+}
+
+// 식별자가 겹칠때의 문제는 사용할때이다.
+namespace BPRO
+{
+    internal class Potion
+    {
+        // B프로그래머
+    }
+}
