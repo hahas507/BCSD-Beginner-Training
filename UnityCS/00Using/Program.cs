d1d61e8d7096bae5d3f5ca97590f9484c4f1a746
hahas507 <aq12361@gmail.com>
2021-04-05 20:28:56 +0900

    03차시

---
+++ UnityCS/00Using/Program.cs
@@ -0,0 +1,3 @@
+﻿using System;
+
+// System 의 기능을 사용한다는 뜻
---
+++ UnityCS/01Class/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//직업
+//레벨
+//스킬
+//체력
+
+// 객체지향*은  저 중에서 적절히 묶어서 클래스화라는 것을 시킨다.
+// 그걸 표현하는것이 클래스
+// class(클래스를 선언하기 위한 키워드) 이름
+// {
+//  클래스 내용
+// }
+
+//*객체지향
+//내가 표현하고 싶은걸 클래스로 만들어
+//그 클래스로 객체를 만드는 프로그래밍.
+//객체를 기반으로 모든걸 해결하기 때문에
+//객체를 지향할 수 밖에 없는 프로그래밍.
+
+internal class Monster
+{
+    //클래스 예시
+}
