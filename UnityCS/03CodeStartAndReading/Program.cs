e926956aaa80d034e90d61eb9dfc2878ca7d8011
hahas507 <aq12361@gmail.com>
2021-04-21 00:48:05 +0900

    4-21

---
+++ UnityCS/03CodeStartAndReading/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03CodeStartAndReading
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            //코드가 실행된다고 함은
+            //어디서부터인가
+            //한줄한줄씩 os(운영체제)가 내 프로그램을 읽어주는것이다. 한줄씩 읽어줌
+            //ex) 윈도우 리눅스 안드로이드
+
+            //사람이 한줄한줄 읽는다는 것이 힘들지만
+            //그걸 ide가 도와준다.
+            //F10을 누르면 됨.
+
+            Console.WriteLine("하하");
+            Console.WriteLine("가나다라");
+        }
+    }
+}
