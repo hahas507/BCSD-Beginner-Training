13ad5ba58521b1a082139a89affe9a2ecf48bb5f
hahas507 <aq12361@gmail.com>
2021-05-10 20:35:31 +0900

    5-10

---
+++ UnityCS/InvenSystem/Inven.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//아이템을 담아두는 인벤토리
+internal class Inven
+{
+    private Item[] ArrItem;
+}
---
+++ UnityCS/InvenSystem/Item.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+internal class Item
+{
+    private string mName;
+    private int mGold;
+
+    public string Name
+    {
+        get
+        {
+            return mName;
+        }
+
+        set
+        {
+            mName = value;
+        }
+    }
+
+    public int Gold
+    {
+        get
+        {
+            return mGold;
+        }
+
+        set
+        {
+            mGold = value;
+        }
+    }
+}
